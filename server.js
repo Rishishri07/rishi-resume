4477ada18c4c465fd80a765aa4501c66881d80fb
unknown <rishishri07@gmail.com>
2023-09-27 21:00:05 +0530

    docs updated

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./docs/rishi-shrivastava'));
+app.use(express.static('./docs/'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'docs/rishi-shrivastava/'}),
+  res.sendFile('index.html', {root: 'docs/'}),
 );
 
 // Start the app by listening on the default Heroku port
